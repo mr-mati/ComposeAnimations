3985eeef78518920ef569db9a5ffbb4bb1e27dab
Mahdi Mohammadi <mahdi60mm60@gmail.com>
2024-05-01 14:34:59 +0330

    AnimatedVisibility

--- app/src/main/java/com/mati/composeanimation/MainActivity.kt
+++ app/src/main/java/com/mati/composeanimation/MainActivity.kt
@@ -3,13 +3,26 @@ package com.mati.composeanimation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.mati.composeanimation.ui.theme.ComposeAnimationTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,11 +31,39 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeAnimationTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
+                Column(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    var isVisible by remember {
+                        mutableStateOf(true)
+                    }
+
+                    AnimatedVisibility(
+                        visible = isVisible, modifier = Modifier
+                            .fillMaxWidth()
+                            .weight(1f)
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .background(Color.Red)
+                                .height(100.dp)
+                        )
+                    }
+
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .align(Alignment.CenterHorizontally)
+                    ) {
+                        Button(onClick = { isVisible = !isVisible }) {
+                            Text(text = "Run Animation")
+                        }
+                        Button(onClick = { }) {
+                            Text(text = "Next Animation")
+                        }
+                    }
+
                 }
             }
         }
@@ -32,8 +73,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = "Hello $name!", modifier = modifier
     )
 }
 
