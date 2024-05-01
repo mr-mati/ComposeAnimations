e57726346e49eddb5f26052501188b6bac09bb56
Mahdi Mohammadi <mahdi60mm60@gmail.com>
2024-05-01 14:38:04 +0330

    create animateIntAsState

--- app/src/main/java/com/mati/composeanimation/MainActivity.kt
+++ app/src/main/java/com/mati/composeanimation/MainActivity.kt
@@ -3,14 +3,15 @@ package com.mati.composeanimation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.animateIntAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,6 +21,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -38,7 +40,20 @@ class MainActivity : ComponentActivity() {
                         mutableStateOf(true)
                     }
 
-                    AnimatedVisibility(
+                    var isRound by remember {
+                        mutableStateOf(false)
+                    }
+
+                    val borderRound by animateIntAsState(targetValue = if (isRound) 100 else 0)
+
+                    Box(
+                        modifier = Modifier
+                            .size(200.dp)
+                            .background(Color.Red)
+                            .clip(RoundedCornerShape(borderRound))
+                    )
+
+                    /*AnimatedVisibility(
                         visible = isVisible, modifier = Modifier
                             .fillMaxWidth()
                             .weight(1f)
@@ -49,7 +64,7 @@ class MainActivity : ComponentActivity() {
                                 .background(Color.Red)
                                 .height(100.dp)
                         )
-                    }
+                    }*/
 
                     Row(
                         modifier = Modifier
